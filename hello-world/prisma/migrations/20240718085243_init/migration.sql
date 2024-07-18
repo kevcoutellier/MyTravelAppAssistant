1384da66a9bcc2fb29b02ea34c909aaf9512818b
Kevin COUTELLIER <kevin.coutellier@epitech.eu>
2024-07-18 11:03:27 +0200

    Register Done

---
+++ hello-world/prisma/migrations/20240718085243_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `user` table. All the data in the column will be lost.
+  - Added the required column `first_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `last_name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `firstName`,
+    DROP COLUMN `lastName`,
+    ADD COLUMN `first_name` VARCHAR(191) NOT NULL,
+    ADD COLUMN `last_name` VARCHAR(191) NOT NULL;
