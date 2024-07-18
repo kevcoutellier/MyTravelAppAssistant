4f25eab2298eecefbe06c5cb4448e4384da03870
Kevin COUTELLIER <kevin.coutellier@epitech.eu>
2024-07-18 15:58:37 +0200

    Back/Front

--- hello-world/start/routes.ts
+++
@@ -1,9 +0,0 @@
-import router from '@adonisjs/core/services/router'
-const AuthController = () => import('../app/controllers/auth_controller.js')
-
-router
-  .group(() => {
-    router.post('/register', [AuthController, 'register'])
-    router.post('/login', [AuthController, 'login'])
-  })
-  .prefix('api/user')
